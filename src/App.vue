558ee2ab6b7dba95530ba5d0ba32c034211b452a
gabrielazevedo-egsys <gabriel.silva@egsys.com.br>
2025-01-27 00:46:23 -0300

    adicionando NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <NavBar />
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
 }
@@ -44,4 +37,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
