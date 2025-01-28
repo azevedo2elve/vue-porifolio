7650db767cb53c1997abbc4da83d3d90f493fb63
gabrielazevedo-egsys <gabriel.silva@egsys.com.br>
2025-01-28 00:05:25 -0300

    zerando o layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,177 +1,99 @@
 <template>
-  <nav class="bg-[#171923] fixed w-full z-20 top-0 start-0">
-    <div class="hidden w-full md:block sm:w-auto" id="navbar-default">
-      <ul
-        class="font-medium flex flex-col items-center p-4 sm:p-0 mt-4 border border-gray-100 rounded-lg sm:flex-row sm:space-x-8 rtl:space-x-reverse sm:mt-0 sm:border-0"
-      >
-        <li>
-          <a
-            href="#Home"
-            class="text-xl block py-2 px-3 text-white rounded hover:bg-[#F33A2F] sm:hover:bg-transparent sm:border-0 sm:hover:text-[#F33A2F] sm:p-0"
-            >Início</a
-          >
-        </li>
-        <li>
-          <a
-            href="#About"
-            class="text-xl block py-2 px-3 text-white rounded hover:bg-[#F33A2F] sm:hover:bg-transparent sm:border-0 sm:hover:text-[#F33A2F] sm:p-0"
-            >Sobre mim</a
-          >
-        </li>
-        <li>
-          <a
-            href="#Skills"
-            class="text-xl block py-2 px-3 text-white rounded hover:bg-[#F33A2F] sm:hover:bg-transparent sm:border-0 sm:hover:text-[#F33A2F] sm:p-0"
-            >Habilidades</a
-          >
-        </li>
-        <li>
-          <a
-            href="#Projects"
-            class="text-xl block py-2 px-3 text-white rounded hover:bg-[#F33A2F] sm:hover:bg-transparent sm:border-0 sm:hover:text-[#F33A2F] sm:p-0"
-            >Projetos</a
-          >
-        </li>
-        <li>
-          <a
-            href="#Contact"
-            class="text-xl block py-2 px-3 text-white rounded hover:bg-[#F33A2F] sm:hover:bg-transparent sm:border-0 sm:hover:text-[#F33A2F] sm:p-0"
-            >Contato</a
-          >
-        </li>
-        <li>
-          <button
-            id="theme-toggle"
-            type="button"
-            class="text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 rounded-lg text-sm p-2.5"
-          >
+  <nav class="bg-gray-800 text-white fixed w-full z-10 shadow-md">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between items-center h-16">
+        <!-- Logo -->
+        <div class="flex-shrink-0">
+          \
+          <img :src="require('@/assets/logo_twelve_square.png')" alt="Logo" class="h-10 w-10" />
+        </div>
+
+        <!-- Nav Links -->
+        <div class="hidden md:flex space-x-4">
+          <a href="#home" class="hover:text-gray-400">Home</a>
+          <a href="#about" class="hover:text-gray-400">About</a>
+          <a href="#skills" class="hover:text-gray-400">Skills</a>
+          <a href="#projects" class="hover:text-gray-400">Projects</a>
+          <a href="#contacts" class="hover:text-gray-400">Contacts</a>
+        </div>
+
+        <!-- Language Switcher -->
+        <div class="hidden md:flex items-center">
+          <button @click="toggleLanguage" class="hover:text-gray-400">
+            {{ currentLanguage }}
+          </button>
+        </div>
+
+        <!-- Mobile Menu Button -->
+        <div class="md:hidden flex items-center">
+          <button @click="toggleMenu" class="text-gray-300 hover:text-white focus:outline-none">
             <svg
-              id="theme-toggle-dark-icon"
-              class="hidden w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
+              v-if="!isMenuOpen"
               xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-            </svg>
-            <svg
-              id="theme-toggle-light-icon"
-              class="hidden w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
             >
               <path
-                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-              ></path>
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 5.25h16.5m-16.5 7.5h16.5m-16.5 7.5h16.5"
+              />
+            </svg>
+            <svg
+              v-else
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-        </li>
-        <li>
-          <div class="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" data-dropdown-toggle="language-dropdown-menu" class="">
-              <svg
-                class="size-10 hover:scale-125 duration-300 rounded-full me-3"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                viewBox="0 0 512 512"
-                style="enable-background: new 0 0 512 512"
-                xml:space="preserve"
-              >
-                viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
-                xml:space="preserve">
-                <circle style="fill: #00c85f" cx="256" cy="256" r="256" />
-                <path
-                  style="fill: #fff046"
-                  d="M246.207,103.892L28.673,248.915c-5.241,3.495-5.241,11.196,0,14.69l217.534,145.022 c5.93,3.954,13.656,3.954,19.586,0l217.534-145.022c5.241-3.495,5.241-11.196,0-14.69L265.793,103.892 C259.863,99.938,252.137,99.938,246.207,103.892z"
-                />
-                <circle style="fill: #4b82e1" cx="256" cy="256" r="105.931" />
-                <path
-                  style="fill: #ffffff"
-                  d="M165.515,201.127c-3.47,5.709-6.403,11.761-8.758,18.111c49.205-3.345,128.166,5.419,197.503,76.211 c2.472-6.151,4.404-12.57,5.692-19.229C292.095,211.143,216.738,199.457,165.515,201.127z"
-                />
-              </svg>
-            </button>
-            <!-- Dropdown -->
-            <div
-              class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
-              id="language-dropdown-menu"
-            >
-              <ul class="py-2" role="none">
-                <li>
-                  <a
-                    href="#"
-                    class="block px-4 pr-2 pt-2 text-sm text-gray-700 hover:text-white hover:bg-[#F33A2F] rounded-lg"
-                    role="menuitem"
-                  >
-                    <div class="inline-flex items-center">
-                      <svg
-                        aria-hidden="true"
-                        class="size-10 rounded-full me-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        id="flag-icon-css-us"
-                        viewBox="0 0 512 512"
-                      >
-                        <g fill-rule="evenodd">
-                          <g stroke-width="1pt">
-                            <path
-                              fill="#bd3d44"
-                              d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                              transform="scale(3.9385)"
-                            />
-                            <path
-                              fill="#fff"
-                              d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                              transform="scale(3.9385)"
-                            />
-                          </g>
-                          <path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)" />
-                          <path
-                            fill="#fff"
-                            d="M8.2 3l1 2.8H12L9.7 7.5l.9 2.7-2.4-1.7L6 10.2l.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7L74 8.5l-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 7.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 24.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 21.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 38.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 35.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 52.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 49.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 66.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 63.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9z"
-                            transform="scale(3.9385)"
-                          />
-                        </g>
-                      </svg>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
-      </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile Menu -->
+    <div v-if="isMenuOpen" class="md:hidden bg-gray-700">
+      <a href="#home" class="block px-4 py-2 hover:bg-gray-600">Home</a>
+      <a href="#about" class="block px-4 py-2 hover:bg-gray-600">About</a>
+      <a href="#skills" class="block px-4 py-2 hover:bg-gray-600">Skills</a>
+      <a href="#projects" class="block px-4 py-2 hover:bg-gray-600">Projects</a>
+      <a href="#contacts" class="block px-4 py-2 hover:bg-gray-600">Contacts</a>
+      <button @click="toggleLanguage" class="block px-4 py-2 hover:bg-gray-600">
+        {{ currentLanguage }}
+      </button>
     </div>
   </nav>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+      currentLanguage: 'EN',
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen
+    },
+    toggleLanguage() {
+      this.currentLanguage = this.currentLanguage === 'EN' ? 'PT' : 'EN'
+    },
+  },
+}
+</script>
+
 <style scoped>
-.navbar {
-  background-color: #333;
-  padding: 1rem;
+nav {
+  background-color: #1f2937; /* Tailwind's gray-800 */
   color: white;
-}
-
-.navbar-list {
-  display: flex;
-  list-style: none;
-  justify-content: space-around;
-  margin: 0;
-  padding: 0;
-}
-
-.navbar-list a {
-  color: white;
-  text-decoration: none;
-  font-weight: bold;
-}
-
-.navbar-list a:hover {
-  text-decoration: underline;
 }
 </style>
